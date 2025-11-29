60023fee8cbe78e6c0c9c14be247e9cbb1bef867
Ramaa <rkramadhevi@gmail.com>
2025-11-29 13:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (35.30%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        int r = sum % k;
+        int r = ((sum % k) + k) % k; // safe modulo => use it when numerator can take negative values, and when result should not be negative
         if(r == 0)
             return 0;
         
